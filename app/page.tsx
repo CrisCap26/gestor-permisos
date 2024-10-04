1323808ac9fad15175b2564287872243514941b7
CrisH <cristopher.26hp@gmail.com>
2024-10-04 17:17:18 -0600

    Quitando archivos y carpetas innecesarias

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <div className='md:grid md:grid-cols-1 md:h-screen'>
       <Header />
-      {/* <Divider className="p-0.5 bg-sky-700" /> */}
       <div className=''>
         <Cards />
       </div>
